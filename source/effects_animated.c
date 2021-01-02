6986a810388d1c986d648d311e41a9a3942dc0c1
William Nguyen <willhnguyen@gmail.com>
2021-01-02 04:58:08 -0800

    WIP: Move myprofiles.h code back into main.c

--- source/effects_animated.c
+++ source/effects_animated.c
@@ -8,12 +8,12 @@
  */
 static uint8_t animatedBreathingValue = 180;
 static int animatedBreathingDirection = -1;
-
+static uint32_t animatedBreathingColor = 0;
 void animatedBreathingInit(led_t *currentKeyLedColors) {
   animatedBreathingValue = 180;
   animatedBreathingDirection = -1;
 }
 
 void animatedBreathing_(led_t *currentKeyLedColors, uint8_t intensity, const uint32_t *colorPalette, const uint32_t colorPaletteLength) {
-  
+  setAllKeysColor(currentKeyLedColors, colorPalette[animatedBreathingColor], intensity);
 }
